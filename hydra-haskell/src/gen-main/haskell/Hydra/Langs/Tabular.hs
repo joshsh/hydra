5157e156c0f1da4c672e259c6bd668711ce772f3
Joshua Shinavier <josh@fortytwo.net>
2023-09-11 11:32:42 -0700

    Update generated Haskell for tier-4 modules

--- hydra-haskell/src/gen-main/haskell/Hydra/Langs/Tabular.hs
+++ hydra-haskell/src/gen-main/haskell/Hydra/Langs/Tabular.hs
@@ -4,9 +4,9 @@ module Hydra.Langs.Tabular where
 
 import qualified Hydra.Core as Core
 import Data.Int
-import Data.List
-import Data.Map
-import Data.Set
+import Data.List as L
+import Data.Map as M
+import Data.Set as S
 
 -- | A data row, containing untyped cells; one per column
 newtype DataRow = 
