867fde80d7af61695e0bc997be14110c57d2eaae
Joshua Shinavier <josh@fortytwo.net>
2023-09-15 11:56:26 -0700

    Move hydra/compute.Kv to hydra/core.Kv in preparation for eliminating annotation parameters

--- hydra-haskell/src/gen-test/haskell/Hydra/Test/TestSuite.hs
+++ hydra-haskell/src/gen-test/haskell/Hydra/Test/TestSuite.hs
@@ -10,7 +10,7 @@ import Data.List as L
 import Data.Map as M
 import Data.Set as S
 
-allTests :: (Testing.TestGroup Compute.Kv)
+allTests :: (Testing.TestGroup Core.Kv)
 allTests = Testing.TestGroup {
   Testing.testGroupName = "All tests",
   Testing.testGroupDescription = Nothing,
