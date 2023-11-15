00d75827a43a5157e3762216b1806458ffa58b89
Joshua Shinavier <josh@fortytwo.net>
2023-11-15 15:28:10 -0800

    Add a couple of DSL convenience functions for pair elimination

--- hydra-haskell/src/main/haskell/Hydra/Dsl/Terms.hs
+++ hydra-haskell/src/main/haskell/Hydra/Dsl/Terms.hs
@@ -67,6 +67,9 @@ field n = Field (FieldName n)
 fieldsToMap :: [Field] -> M.Map FieldName Term
 fieldsToMap fields = M.fromList $ (\(Field name term) -> (name, term)) <$> fields
 
+first :: Term
+first = untuple 2 0
+
 float32 :: Float -> Term
 float32 = literal . Literals.float32
 
@@ -153,6 +156,9 @@ project tname fname = TermFunction $ FunctionElimination $ EliminationRecord $ P
 
 record :: Name -> [Field] -> Term
 record tname fields = TermRecord $ Record tname fields
+
+second :: Term
+second = untuple 2 1
 
 set :: S.Set Term -> Term
 set = TermSet
