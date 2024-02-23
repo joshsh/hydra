0c721e7d27acffe0319f649fb61d8222b7de2cd2
Joshua Shinavier <josh@fortytwo.net>
2024-02-23 11:15:50 -0800

    Rename W.hs to AlgorithmW.hs

--- hydra-haskell/src/main/haskell/Hydra/AlgorithmW.hs
+++ hydra-haskell/src/main/haskell/Hydra/AlgorithmW.hs
@@ -5,7 +5,7 @@
 -- to system F translation by Ryan Wisnesky.
 -- License: Apache 2.0 https://www.apache.org/licenses/LICENSE-2.0 
 
-module Hydra.W where
+module Hydra.AlgorithmW where
 
 import Prelude
 import Control.Monad.Error
