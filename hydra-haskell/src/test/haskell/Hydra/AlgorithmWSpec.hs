c73e8b8e733c8f9e21dff79c86dac2ebda49a027
Joshua Shinavier <josh@fortytwo.net>
2024-02-23 19:45:20 -0800

    Add unit test framework for Algorithm W implementation

---
+++ hydra-haskell/src/test/haskell/Hydra/AlgorithmWSpec.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Hydra.AlgorithmWSpec where
+
+import Hydra.Kernel
+import Hydra.Sources.Libraries
+import Hydra.Inference
+import Hydra.TestUtils
+import Hydra.TestData
+import qualified Hydra.Dsl.Expect as Expect
+import Hydra.Dsl.Terms as Terms
+import qualified Hydra.Dsl.Annotations as Ann
+import qualified Hydra.Dsl.Types as Types
+import Hydra.Dsl.ShorthandTypes
+import Hydra.AlgorithmW
+
+import qualified Test.Hspec as H
+import qualified Test.QuickCheck as QC
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Control.Monad
+
+
+expectType :: Term -> Type -> H.Expectation
+expectType term typ = H.shouldReturn (snd <$> inferWithAlgorithmW term) typ
+
+checkAll :: H.SpecWith ()
+checkAll = H.describe "All test cases" $ do
+  H.it "#1" $ expectType
+    (lambda "x" $ var "x")
+    (Types.lambda "v1" $ Types.function (Types.var "v1") (Types.var "v1"))
+
+spec :: H.Spec
+spec = do
+  checkAll
