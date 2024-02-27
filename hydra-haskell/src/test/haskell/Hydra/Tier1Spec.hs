edbbfbf07637a061cef51815ba53f7ab05c9b7b2
Joshua Shinavier <josh@fortytwo.net>
2024-02-26 16:17:09 -0800

    Add test cases for pre-order and post-order term traversals

--- hydra-haskell/src/test/haskell/Hydra/Tier1Spec.hs
+++ hydra-haskell/src/test/haskell/Hydra/Tier1Spec.hs
@@ -1,4 +1,4 @@
-module Hydra.StripSpec where
+module Hydra.Tier1Spec where
 
 import Hydra.Kernel
 
@@ -10,7 +10,39 @@ import qualified Test.Hspec as H
 import qualified Test.QuickCheck as QC
 import qualified Data.List as L
 import qualified Data.Char as C
+import qualified Data.Maybe as Y
 
+
+checkFoldOverTerm :: H.SpecWith ()
+checkFoldOverTerm = do
+  H.describe "Test foldOverTerm" $ do
+    H.describe "Pre-order" $ do
+      H.it "test #1" $
+        H.shouldBe
+          (traverse TraversalOrderPre node1)
+          ["a"]
+      H.it "test #2" $
+        H.shouldBe
+          (traverse TraversalOrderPre node2)
+          ["a", "b", "c", "d"]
+    H.describe "Post-order" $ do
+      H.it "test #1" $
+        H.shouldBe
+          (traverse TraversalOrderPost node1)
+          ["a"]
+      H.it "test #1" $
+        H.shouldBe
+          (traverse TraversalOrderPost node2)
+          ["b", "d", "c", "a"]
+  where
+    node label children = Terms.pair (Terms.string label) (Terms.list children)
+    labelOf term = case term of
+      TermProduct [TermLiteral (LiteralString label), _] -> Just label
+      _ -> Nothing
+    traverse :: TraversalOrder -> Term -> [String]
+    traverse order = Y.catMaybes . foldOverTerm order (\l t -> l ++ [labelOf t]) []
+    node1 = node "a" []
+    node2 = node "a" [node "b" [], node "c" [node "d" []]]
 
 checkStripTerm :: H.SpecWith ()
 checkStripTerm = do
@@ -38,5 +70,6 @@ checkStripType = do
 
 spec :: H.Spec
 spec = do
+  checkFoldOverTerm
   checkStripTerm
   checkStripType
